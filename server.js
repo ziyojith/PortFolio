588f1ca14b5a4f02ac3295489a4a520a3ac3bd61
JITHIN V J <140269949+ziyojith@users.noreply.github.com>
2025-05-15 22:26:45 +0530

    Update build config for Netlify

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.use(express.urlencoded({extended:true}));
 const cors = require('cors');
 app.use(cors())
 const {MongoClient} = require('mongodb');
-const { use } = require('react');
+
 const url = 'mongodb+srv://Jithin:beast@cluster0.ah3n25x.mongodb.net/PortFolio?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
@@ -23,7 +23,7 @@ app.get('/getAll', async function (req,res) {
     try{
     res.setHeader('content-type','application/json');
     res.setHeader('Access-control-Allow-Origin','*');
-    const db = client.db('Portfolio');
+    const db = client.db('PortFolio');
     const collections = db.collection('Projects');
     const result = await collections.find({},{projection:{_id:0,id:1,Name:1,Detail:1,link:1}}).toArray();
     console.log(result)
@@ -44,7 +44,7 @@ app.get('/insert', async function (req,res) {
     try{
     res.setHeader('content-type','application/json');
     res.setHeader('Access-control-Allow-Origin','*');
-    const db = client.db('Portfolio');
+    const db = client.db('PortFolio');
     const collections = db.collection('Projects');
     const result = await collections.insertOne(req.query)
     console.log(result)
