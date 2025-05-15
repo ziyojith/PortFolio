e4a820d041c8920bc10fc9a4eb74eb5bf9e0f412
JITHIN V J <140269949+ziyojith@users.noreply.github.com>
2025-05-15 21:16:29 +0530

    Update build config for Netlify

--- src/app/nodeitilityservice.service.ts
+++ src/app/nodeitilityservice.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class NodeitilityserviceService {
 
   constructor(private httpclient:HttpClient) {}
-    url:string=" http://localhost:5000/";
+    url:string=" mongodb+srv://Jithin:beast@cluster0.ah3n25x.mongodb.net/PortFolio?retryWrites=true&w=majority";
    display():Observable<any>{
     return this.httpclient.get(this.url + 'getAll')
    }
